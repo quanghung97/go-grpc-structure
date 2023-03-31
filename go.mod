20bcb44a1efced5e93e088abfe220bdf1a2c7434
Amaz005 <ducluongnhan@gmail.com>
2023-03-31 15:38:35 +0700

    add rate_limit to getAllPost method

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.0 // indirect
--- micro1/internal/routes/main.go
+++ micro1/internal/routes/main.go
@@ -1,6 +1,8 @@
 package routing
 
 import (
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+
 	"context"
 	// "github.com/bav-demo/micro1/internal/controllers/society"
 
@@ -20,7 +22,14 @@ type Route struct {
 }
 
 func (r *Route) NewRoute() *grpc.Server {
-	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(middlewares.AuthInterceptor))
+
+	
+	grpcServer := grpc.NewServer(
+		grpc_middleware.WithUnaryServerChain(
+			middlewares.AuthInterceptor,
+			middlewares.UnaryServerInterceptor,
+		),
+	)
 
 	pb.RegisterSocietyServiceServer(grpcServer, r)
 	pbAuthen.RegisterAuthenticationServiceServer(grpcServer, r)
